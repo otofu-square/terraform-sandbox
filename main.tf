d426d495a9c082ec0a081f620058f0b6b417af59
otofu-square <yukita@feedforce.jp>
2017-07-18 23:52:06 +0900

    :+1: Add Internet Gateway, Route Table

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "otofu-tf-vpc" {
   }
 }
 
-resource "aws_subnet" "public_web" {
+resource "aws_subnet" "otofu-public-web" {
   vpc_id                  = "${aws_vpc.otofu-tf-vpc.id}"
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "ap-northeast-1a"
@@ -20,6 +20,29 @@ resource "aws_subnet" "public_web" {
   tags {
     Name = "otofu-tf-public-web"
   }
+}
+
+resource "aws_internet_gateway" "otofu-gw" {
+  vpc_id = "${aws_vpc.otofu-tf-vpc.id}"
+  tags {
+    Name = "otofu-tf-gw"
+  }
+}
+
+resource "aws_route_table" "otofu-public-rtb" {
+  vpc_id = "${aws_vpc.tf_vpc.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+  tags {
+    Name = "otofu-tf-rtb"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = "${aws_subnet.otofu-public-web.id}"
+  route_table_id = "${aws_route_table.otofu-public-rtb.id}"
 }
 
 resource "aws_instance" "otofu-tf-instance" {
