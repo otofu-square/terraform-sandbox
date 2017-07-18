a76b6f5fdf313a3200238408c6c97fd8d71b1719
otofu-square <yukita@feedforce.jp>
2017-07-18 23:31:55 +0900

    :+1: Add subnet

--- main.tf
+++ main.tf
@@ -12,6 +12,16 @@ resource "aws_vpc" "otofu-tf-vpc" {
   }
 }
 
+resource "aws_subnet" "public_web" {
+  vpc_id                  = "${aws_vpc.otofu-tf-vpc.id}"
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+  tags {
+    Name = "otofu-tf-public-web"
+  }
+}
+
 resource "aws_instance" "otofu-tf-instance" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
