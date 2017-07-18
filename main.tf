c9ad10d255fd1bd5817f1deab7b9bd08e52cee8e
otofu-square <yukita@feedforce.jp>
2017-07-18 23:31:36 +0900

    :+1: Add VPC

--- main.tf
+++ main.tf
@@ -2,7 +2,17 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_vpc" "otofu-tf-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags {
+    Name = "otofu-tf-vpc"
+  }
+}
+
+resource "aws_instance" "otofu-tf-instance" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
