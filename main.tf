ab964193b5220a9c8fbb3b8da72af5cee81333da
otofu-square <yukita@feedforce.jp>
2017-07-18 23:54:21 +0900

    :+1: Add security group

--- main.tf
+++ main.tf
@@ -45,6 +45,42 @@ resource "aws_route_table_association" "public_a" {
   route_table_id = "${aws_route_table.otofu-public-rtb.id}"
 }
 
+resource "aws_security_group" "app" {
+  name        = "otofu-tf-web"
+  description = "It is a security group on http of otofu-tf-vpc"
+  vpc_id      = "${aws_vpc.tf_vpc.id}"
+  tags {
+    Name = "tf_web"
+  }
+}
+
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.app.id}"
+}
+
+resource "aws_security_group_rule" "web" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.app.id}"
+}
+
+resource "aws_security_group_rule" "all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.app.id}"
+}
+
 resource "aws_instance" "otofu-tf-instance" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
