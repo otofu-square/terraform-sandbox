b74f5d47a086cace72354c88f0dca71f940a3b96
otofu-square <yukita@feedforce.jp>
2017-07-22 19:13:35 +0900

    :+1: Add private subnet, public subnet

--- aws-basic-server/main.tf
+++ aws-basic-server/main.tf
@@ -10,3 +10,21 @@ resource "aws_vpc" "vpc" {
     Name = "${var.name}_vpc"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id     = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags {
+    Name = "${var.name}_public_subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.2.0/24"
+
+  tags {
+    Name = "${var.name}_private_subnet"
+  }
+}
